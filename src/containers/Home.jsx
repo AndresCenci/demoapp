910433312a15022f3066ca5fb7b6da955f677a50
Andres Cenci <andres.cenci@wenance.com>
2022-06-28 19:13:47 -0300

    routes spinner

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,52 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import Header from '../component/Header';
 import Footer from '../component/Footer';
+import PacmanLoader from "react-spinners/PacmanLoader";
 
 const Home = () => {
+    const [loading, setLoading] = useState(true);
+    const [countries, setCountry]= useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('https://disease.sh/v3/covid-19/countries');
+            const data = await response.json();
+            setCountry(data);
+            setLoading(false);    
+        }
+        fetchData()
+            .catch(console.error);
+    }, []);
+
     return (
         <React.Fragment>
             <Header />
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
+            <PacmanLoader color='#282c34' loading={loading} size={50} />
+            {loading ? '' : <h1>Casos de COVID-19</h1>}
+            {countries.map((country, index) => {
+                return (
+                    <h2 key={index}>País: {country.country} Casos: {country.todayCases}</h2>
+                )
+            })}
             <Footer />
         </React.Fragment>
     )
